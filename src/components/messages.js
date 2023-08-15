4c68d746f5cb3f0617280e06b5b68efae4e90154
Daniela Moreno <dani9292@hotmail.com>
2023-08-15 16:31:06 -0500

    create route

---
+++ src/components/messages.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Message = () => (
+  <h3>messages</h3>
+);
+
+export default Message;
--- src/components/messages.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const messages = () => (
-  <h1>messages</h1>
-);
-
-export default messages;
